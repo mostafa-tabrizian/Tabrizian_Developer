680689b04ff338f32182c76b8dfc2fed0f546a17
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-12-03 11:46:52 +0330

    *pushNotification telegram button just copy the ready text for postman to send manually remove client throw new error *api/admin/client/notification/telegram deleted

--- src/app/(admin)/--admin--/clients/components/pushNotification.tsx
+++ src/app/(admin)/--admin--/clients/components/pushNotification.tsx
@@ -6,17 +6,16 @@ import { toast } from 'react-toastify'
 
 const PushNotification = ({ client }: { client: IClient }) => {
    const [loadingSMS, setLoadingSMS] = useState(false)
-   const [loadingTelegram, setLoadingTelegram] = useState(false)
    const [loadingEmail, setLoadingEmail] = useState(false)
 
    const sendSMS = async () => {
       setLoadingSMS(true)
-      if (!client.mobileNumber) {
-         toast.error('هیج شماره تماسی ثبت نشده است')
-         throw new Error('no mobile number registered')
-      }
 
       try {
+         if (!client.mobileNumber) {
+            return toast.error('هیج شماره تماسی ثبت نشده است')
+         }
+
          const payload = {
             name: client.name,
             mobile: client.mobileNumber,
@@ -47,54 +46,31 @@ const PushNotification = ({ client }: { client: IClient }) => {
    }
 
    const sendTelegram = async () => {
-      setLoadingTelegram(true)
-
       if (!client.telegramId) {
-         toast.error('هیج آیدی تلگرامی ثبت نشده است')
-         throw new Error('no telegram id registered')
+         return toast.error('هیج آیدی تلگرامی ثبت نشده است')
       }
 
-      try {
-         const payload = {
-            telegramId: client.telegramId,
-            name: client.name,
-            domain: client.domain,
-            clientId: client._id,
-         }
+      const textarea = document.createElement('textarea')
+      textarea.value = `سلام ${client.name} عزیز. امیدوارم که حالتون خوب باشه.
+         اعتبار سایت ${client.domain} رو به اتمام هستش. 
+         لطفا جهت تمدید بر لینک زیر کلیک کنید:
+         mostafatabrizian.ir/fa/payment/client/${client._id}`
+      document.body.appendChild(textarea)
+      textarea.select()
+      document.execCommand('copy')
+      document.body.removeChild(textarea)
 
-         const res = await fetch('/api/--admin--/client/notification/telegram', {
-            method: 'POST',
-            body: JSON.stringify(payload),
-         })
-
-         if (!res.ok) throw new Error()
-
-         const resData = await res.json()
-
-         if (resData.message === 'telegramError')
-            return toast.error('در ارسال خطایی رخ داد. لطفا به پشتیبانی اطلاع دهید.')
-         else if (resData.status === 500) {
-            console.log(resData)
-            throw new Error(resData.message)
-         }
-
-         toast.success('پیام تلگرام با موفقیت ارسال شد')
-      } catch (err) {
-         toast.error('در ارسال پیام تلگرام خطایی رخ داد')
-         console.error('api/admin/client/notification/telegram err', err)
-      } finally {
-         setLoadingTelegram(false)
-      }
+      toast.success('پیام تلگرام با موفقیت کپی شد')
    }
 
    const sendEmail = async () => {
       setLoadingEmail(true)
-      if (!client.email) {
-         toast.error('هیج ایمیلی ثبت نشده است')
-         throw new Error('no email id registered')
-      }
 
       try {
+         if (!client.email) {
+            return toast.error('هیج ایمیلی ثبت نشده است')
+         }
+
          const payload = {
             email: client.email,
             name: client.name,
@@ -174,46 +150,23 @@ const PushNotification = ({ client }: { client: IClient }) => {
          </div>
 
          <div>
-            {loadingTelegram ? (
+            <button className='contents' type='button' onClick={sendTelegram}>
                <svg
-                  className='mx-auto mb-3 h-6 w-6 animate-spin text-slate-200'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
+                  className='mx-auto h-8 w-8 text-slate-200'
+                  width='24'
+                  height='24'
                   viewBox='0 0 24 24'
+                  strokeWidth='2'
+                  stroke='currentColor'
+                  fill='none'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                >
-                  <circle
-                     className='opacity-25'
-                     cx='12'
-                     cy='12'
-                     r='10'
-                     stroke='currentColor'
-                     strokeWidth='4'
-                  ></circle>
-                  <path
-                     className='opacity-75'
-                     fill='currentColor'
-                     d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                  ></path>
+                  {' '}
+                  <path stroke='none' d='M0 0h24v24H0z' />{' '}
+                  <path d='M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4' />
                </svg>
-            ) : (
-               <button className='contents' type='button' onClick={sendTelegram}>
-                  <svg
-                     className='mx-auto h-8 w-8 text-slate-200'
-                     width='24'
-                     height='24'
-                     viewBox='0 0 24 24'
-                     strokeWidth='2'
-                     stroke='currentColor'
-                     fill='none'
-                     strokeLinecap='round'
-                     strokeLinejoin='round'
-                  >
-                     {' '}
-                     <path stroke='none' d='M0 0h24v24H0z' />{' '}
-                     <path d='M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4' />
-                  </svg>
-               </button>
-            )}
+            </button>
             <span className='text-sm'>telegram</span>
          </div>
 
